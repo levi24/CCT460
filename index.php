c71dc4344bbedfda770e9e9e483080df80a697b3
levi24 <srivrac24@gmail.com>
2015-07-08 14:38:36 -0400

    index file
    Added a new index file

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lecture 1 Homework</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="center">
+		<div id="top">
+			<h1>Superfab Office Products - Office Supply Products Division</h1>
+		</div>
+		<div class="menu">
+		<ul>
+		<li><a href="https://phoenix.sheridanc.on.ca/~ccit2704/Lecture1/index.php">About Us</a></li>
+		<li><a href="https://phoenix.sheridanc.on.ca/~ccit2704/Lecture1/template.php">Contact Us</a></li>
+		</ul>
+		</div>
+		<div class="main">
+			<div id="content">
+			<p>The Superfab name has come to mean quality and innovation in advanced office 
+			solutions. Never mind mundane office offerings for back-room pencil-pushing 
+			geeks ... Superfabofferings are smart, slick, and so far ahead of its time that 
+			it's well worth the outrageous prices we demand for them.</p>
+
+			<p>Our award-winning products include: MegalaMartTM, PATTM, Hinternet
+			HunterTM, DAISYTM, and BugSmasher SweetTM, plus other amazing devices that are
+			so secret, if we told you about them we'd then have to swear you to secrecy
+			and degauss your hard drive.</p>
+
+			<p>The biggest corporations all over the world practically flock to our door
+			to have their most ferocious technological challenges turned into purring
+			pussycats by the boy brainiacs and girl geniuses on our staff.</p>
+
+			<p>The Superfab laboratory is located in Sasquatch Falls, Washington (the
+			state whose rich negative ion levels (spawned by high annual rainfall levels)
+			and scenic ocean views seem to inspire technogeeks and propeller-heads to
+			awesome levels of coding productivity ... not that their cubicles actually
+			have a window, or view of any kind).</p>
+
+			<p>The lab is presided over by the legendary Dr. Murry Duchance, PhD, winner
+			of the 1995 Nobel Prize in Physics, 1996 National Science Foundation gold
+			medal, 1997 Thomas Alva Edison Prize, and the 1998 Grammy Award for best new
+			rap album.</p>
+
+			<p>But Dr. Duchance is only one of our many brainiacs.  With so many
+			unmitigated geniuses on the payroll, often stumbling over one another,
+			Superfab likes to get them out of the office from time to time (otherwise they
+			might forget how to tie their shoelaces, and perform other simple social
+			functions that the rest of us take for granted).  Ask us about of in-company
+			supply program!  We'll develop a custom program to your specifications, or
+			simply show up as consultants and dazzle you with our brilliant thinking, and
+			no doubt leave you depressed by your own stupidity.</p> </div>			
+		</div>
+		
+		</div>
+	
+	<footer>	
+	<?php	
+	echo "Rachit Srivastava " . "<br/>" . "<br/>" . "Today&apos;s date is: " . date('m - d - 20y');
+	?>
+	</footer>
+	</body>
+</html>
